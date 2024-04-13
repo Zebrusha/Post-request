af9b5d2c6c2d95844d0d0a4793d070d9f72c6527
Anastasia Voronchikhina <zebrushaa@mail.ru>
2024-04-13 19:21:18 +0300

    Initial commit

---
+++ src/test/java/PostRequestTestV1.java
@@ -0,0 +1,25 @@
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+
+public class PostRequestTestV1 {
+    @Test
+    void shouldSendDemoAccount() {
+
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+
+                .when()
+                .post("/post")
+
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"))
+
+        ;
+    }
+}
